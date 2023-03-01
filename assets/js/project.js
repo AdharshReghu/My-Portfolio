5f53d8ac7f4f041b80ceecd33591c87b36079cf0
Adharsh Reghu <76521154+AdharshReghu@users.noreply.github.com>
2023-03-02 00:43:22 +0530

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -40,7 +40,7 @@ const projects = [
   },
  {
     title: "QuizzApp",
-    cardImage: "assets/images/project-page/QuizIcon.jpg",
+    cardImage: "assets/images/project-page/QuizIcon.png",
     description: "A simple true or false based quiz app with a predefined set of questions",
     tagimg: "https://img.freepik.com/premium-vector/vector-illustration-hand-drawn-doodle-two-white-dice-with-contour-gambling-symbol_501907-729.jpg?w=2000",
     Previewlink: "https://github.com/AdharshReghu/QuizzApp",
